8bc2f6877533ebd7f69d87fc111b83d45a3aa6d0
Adil <madilaslam9@gmail.com>
2024-08-16 00:32:46 +0500

    done with setting html

--- html/file.js
+++ html/file.js
@@ -1,13 +1,37 @@
-import { Web3 } from "web3";
+import { eth, Web3 } from "web3";
+
+
+// HTML Elements
+let addressArray = document.getElementById("addresses")
 
 
 
-const connect = async () => {
-    let web3 = new Web3.providers(Web3.givenProvider)
-    console.log(web3)
-    console.log("connected")
+
+//Being in the testing environment, we'll be using Anvil
+let anvil = true;
+let provider = anvil
+  ? new Web3.providers.HttpProvider("http://127.0.0.1:8545")
+  : new Web3.providers.HttpProvider(window.ethereum) ||
+    new Web3.givenProvider();
+
+let addresses
+let wallet
+// creating a WEB 3 JS instance for interacting with blockchain
+let web3 = new Web3(provider)
+
+const main = async () => {
+ wallet = web3.wallet.create()
+ addresses = await web3.eth.getAccounts()
+ addressArray.innerHTML = addresses
+ 
 }
 
+main()
 
 
-window.connect = connect
+//adding address to the wallet
+
+
+
+//first :- with the help of getAccounts get all of the accounts printed onto the screen
+//second :- when a user clicks onto that account select it as the default account that will be used for signing the transactions
